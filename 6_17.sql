093766b84c1972557161646c20b5c03b0a5b9364
slowpoke <wuchaoxu1006@gmail.com>
2024-06-19 17:52:24 +0800

    question 4-6

--- 6_17.sql
+++ 6_17.sql
@@ -11,3 +11,18 @@ WHERE ListPrice <> 0
 SELECT ProductID, Name, Color, ListPrice
 FROM Production.Product
 WHERE Color is NULL
+
+-- QUESTION 4
+SELECT ProductID, Name, Color, ListPrice
+FROM Production.Product
+WHERE Color is NOT NULL
+
+-- QUESTION 5
+SELECT ProductID, Name, Color, ListPrice
+FROM Production.Product
+WHERE Color is NOT NULL AND ListPrice > 0
+
+-- QUESTION 6
+SELECT Name + ' ' + Color
+FROM Production.Product
+WHERE Color is NOT NULL
