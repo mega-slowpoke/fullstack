2975b94fe9f194278360161639e75759f8fd1d37
slowpoke <wuchaoxu1006@gmail.com>
2024-06-26 21:22:04 +0800

    finish 3.2

---
+++ June25/Question3_2/Ball.cs
@@ -0,0 +1,37 @@
+﻿namespace Main.Question3_2;
+
+public class Ball
+{
+    public int Size { get; set; }
+    public Color BallColor { get; set; }
+    
+    public int ThrowCnt { get; set; }
+
+    private int State = 1; // 1 -> never poped, 0 -> poped
+    
+    public Ball(int size, Color ballColor)
+    {
+        Size = size;
+        BallColor = ballColor;
+        ThrowCnt = 0;
+    }
+
+    public void Pop()
+    {
+        Size = 0;
+        State = 0;
+    }
+    
+    public void Throw()
+    {
+        if (State == 1)
+        {
+            ThrowCnt++;
+        }
+    }
+
+    public int Count()
+    {
+        return ThrowCnt;
+    }
+}
---
+++ June25/Question3_2/Color.cs
@@ -0,0 +1,17 @@
+﻿namespace Main.Question3_2;
+
+public class Color
+{
+    public int Red { get; set; }
+    public int Green { get; set; }
+    public int Blue { get; set; }
+    public int Alpha { get; set; }
+    
+    public Color(int red, int green, int blue, int alpha = 255)
+    {
+        Red = red;
+        Green = green;
+        Blue = blue;
+        Alpha = alpha;
+    }
+}
