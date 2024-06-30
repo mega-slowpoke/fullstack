1a43841f87dbf5b43874ee50b68673cfa355df2e
slowpoke <wuchaoxu1006@gmail.com>
2024-06-30 23:22:10 +0800

    finish day 2 review

--- Evaluation/Day2Demo.cs
+++ Evaluation/Day2Demo.cs
@@ -2,5 +2,47 @@
 
 public class Day2Demo
 {
+    // Encapsulation
+    // Inheritance
+    // Abstraction
+    // Polymorphism : Compile-time -> overloading / overriding 
+    //                Runtime      -> Upcast
+}
+
+// abstract / virtual to be overridden
+
+
+
+
+// static class is "static", it cannot be inherited, initialized and only contains static stuff
+// abstract class -> cannot be initialized, sealed class -> cannot be inherited, other than that, they are just normal classes
+public static class StaticClassTest
+{
+    public static void StaticMethod() {
+        Console.WriteLine("This is a static method in a static nested class.");
+    }
     
+}
+
+
+// add a customized method to 
+public static class ExtensionDemo
+{
+    public static bool IsPan(this string s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            if (s[left] != s[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
 }
--- Evaluation/Program.cs
+++ Evaluation/Program.cs
@@ -7,3 +7,5 @@ Console.WriteLine(demo1.MyAdd(2, 3, 4, 1, 12));
 Console.WriteLine(demo1.OptionalVal(7,12));
 int returnVal1 = demo1.OutTest(5, out int returnVal2, out int returnVal3);
 Console.WriteLine(returnVal1 + " " + returnVal2 + " " + returnVal3);
+
+Console.WriteLine("abba".IsPan());
